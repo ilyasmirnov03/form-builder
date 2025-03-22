4be1d396d843b3b7b3987cc169fcbf1ea1124ec7
Ilya Smirnov <ilua.smirnof@gmail.com>
2025-03-22 12:03:20 +0100

    test(form-builder): Run tests with actual DOM
    Run tests with actual DOM environment

--- test/form-builder.test.ts
+++ test/form-builder.test.ts
@@ -1,10 +1,36 @@
 import {FormBuilder} from "../lib";
+import { JSDOM } from 'jsdom';
+
+const domWithoutForm = `<!DOCTYPE html><html><body><div></div></body></html>`;
+const domWithForm = `<!DOCTYPE html><html><body><form class="form"></form></body></html>`;
 
 describe('FormBuilder', () => {
   test('Should throw on unmatched selector', () => {
     expect(() => {
-      // TODO: actually provide a valid DOM to run the querySelector on.
+      const dom = new JSDOM(domWithoutForm);
+      (global as any).document = dom.window.document;
+
       new FormBuilder('.form', {});
     }).toThrow();
-  })
+  });
+
+  test('Should not throw on matched selector', () => {
+    expect(() => {
+      const dom = new JSDOM(domWithForm);
+      (global as any).document = dom.window.document;
+      (global as any).HTMLFormElement = dom.window.HTMLFormElement;
+
+      new FormBuilder('.form', {});
+    }).not.toThrow();
+  });
+
+  test('Should throw when provided selector isn\'t an HTMLFormElement', () => {
+    expect(() => {
+      const dom = new JSDOM(domWithoutForm);
+      (global as any).document = dom.window.document;
+      (global as any).HTMLFormElement = dom.window.HTMLFormElement;
+
+      new FormBuilder('div', {});
+    }).toThrow(TypeError);
+  });
 });
