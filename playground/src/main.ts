a7e7907f3c64103debc65bc518d5c8ed27318947
ilyasmirnov03 <ilua.smirnof@gmail.com>
2025-06-19 00:29:48 +0200

    feat(playground): Add more html inputs
    Add more html inputs for testing

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,6 +1,11 @@
 import { FormBuilder, FormField } from '../../lib';
 
 const fb = new FormBuilder('.form', {
-  name: new FormField('')
+  name: new FormField('mario'),
+  amount: new FormField(0),
+  job: new FormField('programmer'),
+  description: new FormField('very long description'),
+  happy: new FormField(true),
+  meal: new FormField('soup'),
 });
-console.log(fb.fields.name.value);
+console.log(fb.fields);
